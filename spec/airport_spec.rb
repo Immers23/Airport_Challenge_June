4758a91f74c794e1b2a28efd02233f896fc976d5
Imraan Ali <imraan91@hotmail.co.uk>
2020-06-05 18:25:54 +0100

    User Story 2 As an air traffic controller So I can get passengers on the way to their destination I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,35 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
 
   subject(:airport) { described_class.new }
 
-  it 'instracts a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    it 'instructs a plane to land' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+
+    it 'adds a plane into the planes array' do
+      plane = Plane.new
+      airport.land(plane)
+      expect(airport.planes).to eq [plane]
+    end
   end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+
+    it 'removes the plane from the plane array' do
+      plane = Plane.new
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.planes).to eq []
+    end
+  end
+
+
 
 end
